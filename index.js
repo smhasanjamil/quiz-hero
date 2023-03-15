ffd044c0f3db9042bcbb91769105a44721ae3883
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-16 00:40:54 +0600

    Quiz section display problem solved

--- index.js
+++ index.js
@@ -60,20 +60,22 @@ const displayQuiz = (data) => {
     return;
   }
 
-  data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+data.forEach((quiz, i) => {
+  quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+<div class="flex items-center">
+  <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+    ${i + 1}
   </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
+  <p class="text-gray-800 text-sm">${quiz.question}</p>
+</div>
+<div class="grid grid-cols-2 gap-4 mt-5">
+  ${displayQuizOptions(quiz.options, i)}
+</div>
 </div>`;
-  });
+});
 };
+
+
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
