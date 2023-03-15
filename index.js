a42ca18c5f3e2a0b03e0323c7fa2df0275955f8f
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-16 01:04:20 +0600

    Set and get data from local storage

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ startQuiz.addEventListener("click", () => {
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
   const data = await res.json();
-  // const data = await res.json();
 
   quizData = data;
   displayQuiz(data);
@@ -60,8 +59,8 @@ const displayQuiz = (data) => {
     return;
   }
 
-data.forEach((quiz, i) => {
-  quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+  data.forEach((quiz, i) => {
+    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
 <div class="flex items-center">
   <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
     ${i + 1}
@@ -72,7 +71,7 @@ data.forEach((quiz, i) => {
   ${displayQuizOptions(quiz.options, i)}
 </div>
 </div>`;
-});
+  });
 };
 
 
